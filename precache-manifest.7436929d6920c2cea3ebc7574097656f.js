a86244e0b177929a63e76fa3868a5d6a8608b0b8
valderli <derlly@ipam.org.br>
2020-06-24 15:12:30 -0300

    Updates

---
+++ precache-manifest.7436929d6920c2cea3ebc7574097656f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c6c871e250da85169b90730b94426f4",
+    "url": "/somai-dashboard/frontend/index.html"
+  },
+  {
+    "revision": "5a442c16a3841ade01e3",
+    "url": "/somai-dashboard/frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "496e0001d012b5cf6bd0",
+    "url": "/somai-dashboard/frontend/static/js/2.24fc2041.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/somai-dashboard/frontend/static/js/2.24fc2041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a442c16a3841ade01e3",
+    "url": "/somai-dashboard/frontend/static/js/main.401fdc08.chunk.js"
+  },
+  {
+    "revision": "47a184cab7f5f4391e4b",
+    "url": "/somai-dashboard/frontend/static/js/runtime-main.f83bf5bf.js"
+  }
+]);
