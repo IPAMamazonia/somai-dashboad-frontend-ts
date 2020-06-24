f4d7ddb8daeab9a05c898dac927879b957b7e3ce
valderli <derlly@ipam.org.br>
2020-06-24 20:09:00 -0300

    heroku

---
+++ build/precache-manifest.18f6423f4be574953705e2920d26bc9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d729827b830ec9459c62edef215180b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a442c16a3841ade01e3",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d307dd1587ffe2027881",
+    "url": "/static/js/2.d31da488.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/static/js/2.d31da488.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a442c16a3841ade01e3",
+    "url": "/static/js/main.401fdc08.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
